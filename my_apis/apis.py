81195a61a46433a0e5ec90b2ccef72a02aae2afe
Adina <adinaazhar@gmail.com>
2025-08-11 06:16:27 +0500

    Add my_apis folder

---
+++ my_apis/apis.py
@@ -0,0 +1,7 @@
+import sys
+
+gemini_key = "AIzaSyBOk3nBcXgTZA0yh9bGv3cNtyECTVjlu_Q"
+
+pinecone_Api_vector_db = (
+    "pcsk_4Q1x5a_K43s635SYEnTdfqbdtGR1V6RUC2miYxCwENDSwvjJ7WWCxsPcofhheur4jRkHwV"
+)
