db005c281ccf7a520ab4493b428a4f91b8da1faa
Adina <adinaazhar@gmail.com>
2025-08-21 20:07:53 +0500

    Adding new paths

--- my_apis/apis.py
+++ my_apis/apis.py
@@ -1,7 +1,10 @@
 import sys
 
-gemini_key = "AIzaSyBOk3nBcXgTZA0yh9bGv3cNtyECTVjlu_Q"
+
+gemini_key = "AIzaSyDpBd5CuSUaejX_XBb1LUdgYM4evDHLk3Y"
 
 pinecone_Api_vector_db = (
     "pcsk_4Q1x5a_K43s635SYEnTdfqbdtGR1V6RUC2miYxCwENDSwvjJ7WWCxsPcofhheur4jRkHwV"
 )
+# gemini_key = "AIzaSyD4wzxxopaAFLpL3WFtk9-M6afpk-e0IYo"
+# gemini_key = "AIzaSyBy-ZeUjxz83MBz5vdPNIPEp1NF3T9py1c"
